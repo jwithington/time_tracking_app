835a18cee3c0d3579ef105fdf97c2ad0a70611b6
Jim Withington <jamesrwithington@gmail.com>
2019-07-14 19:48:44 -0700

    add TimersDashboard

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,14 @@
-
+class TimersDashboard extends React.component {
+  render() {
+    return (
+      <div className="ui three column centered grid">
+        <div className="column">
+          <EditableTimerList />
+          <ToggleableTimerForm
+            isOpen={true} // when open, the form is being displayed
+          />
+        </div>
+      </div>
+    );
+  }
+}
