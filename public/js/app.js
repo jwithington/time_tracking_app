67da7464bd84d940bcc13826f7dec5dea68d3f3e
Jim Withington <jamesrwithington@gmail.com>
2019-07-15 09:55:00 -0700

    add Timer

--- public/js/app.js
+++ public/js/app.js
@@ -20,7 +20,7 @@ class EditableTimerList extends React.Component {
         <EditableTimer
           title='Learn React'
           project='Web Learning'
-          elapsed='8986300'
+          elapsed='8986300' // in milliseconds because thats what JS uses for time  
           runningSince={null}
           editFormOpen={false}
         />
@@ -107,3 +107,33 @@ class ToggleableTimerForm extends React.Component {
   }
 }
 
+class Timer extends React.Component {
+  render() {
+    const elapsedString = helpers.renderElapsedString(this.props.elapsed); // in helpers.js
+    return (
+      <div className="ui centered card">
+        <div className="content">
+          <div className="header">
+            {this.props.title}
+          </div>
+          <div className="meta">
+            {this.props.project}
+          </div>
+          <div className="center aligned description">
+            <h2>
+              {elapsedString}
+            </h2>
+          </div>
+        </div>
+        <div className="extra content">
+          <span className="right floated edit icon">
+            <i className="trash icon" />
+          </span>
+        </div>
+        <div className="ui bottom attached blue basic button">
+          Start
+        </div>
+      </div>
+    );
+  }
+}
