15644172422cb3cc637a9a265e3a003a4a841417
Jim Withington <jamesrwithington@gmail.com>
2019-07-15 09:25:21 -0700

    add another static timer and add EditableTimer

--- public/js/app.js
+++ public/js/app.js
@@ -22,9 +22,39 @@ class EditableTimerList extends React.component {
           project='Web Learning'
           elapsed='8986300'
           runningSince={null}
+          editFormOpen={false}
+        />
+        <EditableTimer 
+          title='Learn Extreme Ironing'
+          project='Domestic'
+          elapsed='389095'
+          runningSince={null}
           editFormOpen={true}
         />
       </div>
     );
+  }
+}
+
+class EditableTimer extends React.component {
+  render() {
+    
+    if (this.props.editFormOpen) {
+      return (
+        <TimerForm 
+          title={this.props.title} // both of these mean that the current value is pulled in when editing
+          project={this.props.project}
+        />
+      );
+    } else {
+      return (
+        <Timer 
+          title={this.props.title}
+          project={this.props.project}
+          elapsed={this.props.elapsed}
+          runningSince={this.props.runningSince}
+        />
+      );
+    }
   }
 }
